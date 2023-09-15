93f41897221d467ef73443d80105bae0ecff11a9
TheRealShreyash <koshta.shreyash02@gmail.com>
2023-09-15 23:31:21 +0530

    fixed get_trivia.py | success

--- examples/Python/get_trivia.py
+++ examples/Python/get_trivia.py
@@ -9,10 +9,10 @@ def get_trivia():
     if res.status_code == 200:
         data = res.json()
         question = data["question"]
-        op1 = data["option1"]
-        op2 = data["option2"]
-        op3 = data["option3"]
-        cop = data["correct_option"]
+        op1 = data["1"]
+        op2 = data["2"]
+        op3 = data["3"]
+        cop = data["correct"]
         return question, op1, op2, op3, cop
     else:
         error = (
@@ -29,11 +29,13 @@ async def get_trivia_():
             if res.status == 200:
                 data = await res.json()
                 question = data["question"]
-                op1 = data["option1"]
-                op2 = data["option2"]
-                op3 = data["option3"]
-                cop = data["correct_option"]
+                op1 = data["1"]
+                op2 = data["2"]
+                op3 = data["3"]
+                cop = data["correct"]
                 return question, op1, op2, op3, cop
             else:
                 error = "STATUS_CODE != 200\nThe API is not responding to this endpoint currently."
                 return error
+
+print(get_trivia())
